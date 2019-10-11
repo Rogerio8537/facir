e198724465f9c58d033ec706a579d87081adaa2a
Rogerio8537 <54038270+Rogerio8537@users.noreply.github.com>
2019-10-10 21:06:38 -0300

    Update l6q1.c

--- lista6resolvida/l6q1.c
+++ lista6resolvida/l6q1.c
@@ -1,1 +1,18 @@
+#include<stdio.h>
+
+/*l6q1*/
+int main()
+{
+    char nome[50];
+    int x;
+
+    printf("Insira uma mensagem:");
+    gets(nome);
+    x=nome;
+
+     printf("Voce escreveu:%s",x);
+
+    return 0;
+
+}
 
