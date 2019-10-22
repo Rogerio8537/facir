0728bb29f4ffacac39ef1b0f775e5b66a211fc0c
Rogerio8537 <54038270+Rogerio8537@users.noreply.github.com>
2019-10-22 08:58:16 -0300

    Add files via upload

---
+++ lista6resolvida/l6q9.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main() {
+  char nome[20];
+  int i;
+  printf("Insira uma string: ");
+  gets(nome);
+  for (i=0;nome[i] != '\0';i++) {
+      if(nome[i]=='0'){
+        nome[i]='1';
+      }
+  }
+  printf("%s",nome);
+}
+
